2f0daf84d6d755f9ee02c56d0ec973a7c22a4c1c
Nathaniel Soriano <nathaniel.valdez.soriano@gmail.com>
2023-04-02 21:39:47 -0400

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,12 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        #returns True even if pub_date is in the future
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+        
 
 
 class Choice(models.Model):
